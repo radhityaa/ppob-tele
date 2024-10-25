4d7af232b52759134ebff6ebadc9fbd6fd01b223
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-10-25 14:49:54 +0700

    update

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -8,11 +8,21 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use Telegram\Bot\Api;
 
 class CallbackController extends Controller
 {
+    protected $telegram;
+
+    public function __construct(Api $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
     public function callbackTripay(Request $request)
     {
+        $responseTele = $this->telegram->getWebhookUpdate();
+
         $callbackSignature = $request->server('HTTP_X_CALLBACK_SIGNATURE');
         $json = $request->getContent();
         $signature = hash_hmac('sha256', $json, TripayHelper::getPrivateKey());
@@ -64,14 +74,51 @@ class CallbackController extends Controller
                     $user->update([
                         'saldo' => $user->saldo + $data->amount_received
                     ]);
+
+                    $saldo = number_format($user->saldo, 0, '.', '.');
+
+                    $nominalFormatted = number_format($deposit->nominal, 0, '.', '.');
+                    $feeFormatted = number_format($deposit->fee, 0, '.', '.');
+                    $totalFormatted = number_format($deposit->total, 0, '.', '.');
+                    $amountReceivedFormatted = number_format($deposit->amount_received, 0, '.', '.');
+                    $paidAt = $deposit->paid_at->format('d-m-Y H:i:s');
+
+                    $this->telegram->sendMessage([
+                        'chat_id' => $user->chat_id,
+                        'text' => "Hallo, $user->name\nTerima Kasih! Pembayaran telah kami terima.\nSaldo anda sekarang: $saldo\n\nDetail Deposit:\n\n- Invoice: $deposit->invoice\n- Metode: $deposit->method\n- Total: $totalFormatted\n- Fee: $feeFormatted\n-Nominal: $nominalFormatted\n- Saldo Diterima: $amountReceivedFormatted\n- Waktu Dibayarkan: $paidAt\n- Status: paid\n\nSilahkan Cek kembali saldo anda, kirim dengan format: Cek Saldo .",
+                    ]);
                     break;
 
                 case 'EXPIRED':
+                    $user = User::where('id', $deposit->user_id)->first();
                     $deposit->update(['status' => 'failed']);
+
+                    $nominalFormatted = number_format($deposit->nominal, 0, '.', '.');
+                    $feeFormatted = number_format($deposit->fee, 0, '.', '.');
+                    $totalFormatted = number_format($deposit->total, 0, '.', '.');
+                    $amountReceivedFormatted = number_format($deposit->amount_received, 0, '.', '.');
+                    $exp = $deposit->expired_at->format('d-m-Y H:i:s');
+
+                    $this->telegram->sendMessage([
+                        'chat_id' => $user->chat_id,
+                        'text' => "Hallo, $user->name.\nMohon Maaf! Pembayaran anda telah Expired atau Sudah Kadaluarsa\nSilahkan lalukan deposit ulang\n\nDetail Deposit:\n\n- Invoice: $deposit->invoice\n- Metode: $deposit->method\n- Total: $totalFormatted\n- Fee: $feeFormatted\n-Nominal: $nominalFormatted\n- Saldo Diterima: $amountReceivedFormatted\n- Expired: $exp\n\n- Status: failed\n\nTerima Kasih.",
+                    ]);
                     break;
 
                 case 'FAILED':
+                    $user = User::where('id', $deposit->user_id)->first();
                     $deposit->update(['status' => 'failed']);
+
+                    $nominalFormatted = number_format($deposit->nominal, 0, '.', '.');
+                    $feeFormatted = number_format($deposit->fee, 0, '.', '.');
+                    $totalFormatted = number_format($deposit->total, 0, '.', '.');
+                    $amountReceivedFormatted = number_format($deposit->amount_received, 0, '.', '.');
+                    $exp = $deposit->expired_at->format('d-m-Y H:i:s');
+
+                    $this->telegram->sendMessage([
+                        'chat_id' => $user->chat_id,
+                        'text' => "Hallo, $user->name\nMohon Maaf! Pembayaran anda gagal diproses.\nSilahkan lalukan deposit ulang.\n\nDetail Deposit:\n\n- Invoice: $deposit->invoice\n- Metode: $deposit->method\n- Total: $totalFormatted\n- Fee: $feeFormatted\n-Nominal: $nominalFormatted\n- Saldo Diterima: $amountReceivedFormatted\n- Expired: $exp\n- Status: failed\n\nTerima Kasih.",
+                    ]);
                     break;
 
                 default:
