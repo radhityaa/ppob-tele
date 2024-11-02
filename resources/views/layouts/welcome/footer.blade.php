05caea7021d06fdd4aec368bdc2f53da535391ae
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-11-03 00:18:13 +0700

    update

--- resources/views/layouts/welcome/footer.blade.php
+++ resources/views/layouts/welcome/footer.blade.php
@@ -5,19 +5,10 @@
             <script>
                 document.write(new Date().getFullYear());
             </script>
-            , made with ❤️ by
-            <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+            PT. Ayasya Tech Indonesia
         </div>
         <div>
-            <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-            <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More
-                Themes</a>
-
-            <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank"
-                class="footer-link me-4">Documentation</a>
-
-            <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
-                class="footer-link me-4">Support</a>
+            <a href="https://t.me/atippob" class="footer-link me-4" target="_blank">Telegram</a>
         </div>
     </div>
 </footer>
